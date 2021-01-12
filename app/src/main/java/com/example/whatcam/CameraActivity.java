4da2e91d6b9d87c1668dfe7cf9864dd5c2c0ace4
abhayjat <abhay.jat@outlook.com>
2021-01-12 14:32:41 +0530

    CameraActivity make Workable

--- app/src/main/java/com/example/whatcam/CameraActivity.java
+++ app/src/main/java/com/example/whatcam/CameraActivity.java
@@ -1,14 +1,144 @@
 package com.example.whatcam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.extensions.HdrImageCaptureExtender;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
+    private int REQUEST_CODE_PERMISSIONS = 1000;
+    private final String[] REQUEST_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private Executor executor = Executors.newSingleThreadExecutor();
+
+    PreviewView mPreviewView;
+    LinearLayout captureImage;
+
+    static byte[] outputImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputImage = null;
+        mPreviewView = findViewById(R.id.camera);
+        captureImage = findViewById(R.id.captureImage);
+
+        if (allPermissionsGrants()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUEST_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .build();
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
+
+        if (hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)) {
+            hdrImageCaptureExtender.enableExtension(cameraSelector);
+        }
+
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+
+
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis, imageCapture);
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageCapture.takePicture(executor, new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy image) {
+                        outputImage = toBitmap(image);
+                        Intent intent = new Intent(CameraActivity.this, RecognitionActivity.class);
+                        startActivity(intent);
+                        super.onCaptureSuccess(image);
+                    }
+                });
+            }
+        });
+    }
+
+    private byte[] toBitmap(ImageProxy imageProxy) {
+        ByteBuffer byteBuffer = imageProxy.getPlanes()[0].getBuffer();
+        byteBuffer.rewind();
+        byte[] bytes = new byte[byteBuffer.capacity()];
+        byteBuffer.get(bytes);
+        byte[] clonedBytes = bytes.clone();
+        return clonedBytes;
+    }
+
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                }catch (ExecutionException | InterruptedException e){
+
+                }
+            }
+        },ContextCompat.getMainExecutor(this));
+    }
+
+    private boolean allPermissionsGrants() {
+        for (String permission : REQUEST_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGrants()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permission not granted by user", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
     }
 }
